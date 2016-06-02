c81901b1c7be75018a4194430b25fdc3b657d554
unknown <11204785@W1818046.portoalegre.pucrsnet.br>
2016-06-01 22:31:28 -0300

    <Adicionando teste unitarios para cada grafo>

--- Grafos/Grafo/test/GrafoTeste.java
+++ Grafos/Grafo/test/GrafoTeste.java
@@ -4,6 +4,9 @@
  * and open the template in the editor.
  */
 
+import grafo.Grafo;
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -16,29 +19,74 @@ import static org.junit.Assert.*;
  * @author 11204785
  */
 public class GrafoTeste {
-    
-    public GrafoTeste() {
-    }
-    
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
-    @Before
-    public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
+
+    @Test
+    public void testg1() throws Exception {
+        Grafo g = new Grafo("g1.txt");
+        List<Integer> expected = new ArrayList<>();
+        expected.add(0);
+        List<Integer> actual = g.percursoLargura(0);
+        assertEquals(expected, actual);
+
     }
 
-    // TODO add test methods here.
-    // The methods must be annotated with annotation @Test. For example:
-    //
-    // @Test
-    // public void hello() {}
+    @Test
+    public void testg2() throws Exception {
+        Grafo g = new Grafo("g2.txt");
+        List<Integer> expected = new ArrayList<>();
+        expected.add(0);
+        List<Integer> actual = g.percursoLargura(0);
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void testg3() throws Exception {
+        Grafo g = new Grafo("g3.txt");
+        List<Integer> expected = new ArrayList<>();
+        expected.add(0);
+        expected.add(1);
+        List<Integer> actual = g.percursoLargura(0);
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void testg4() throws Exception {
+        Grafo g = new Grafo("g4.txt");
+        List<Integer> expected = new ArrayList<>();
+        expected.add(0);
+        List<Integer> actual = g.percursoLargura(0);
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void testg5() throws Exception {
+        Grafo g = new Grafo("g5.txt");
+        List<Integer> expected = new ArrayList<>();
+        expected.add(0);
+        expected.add(4);
+        List<Integer> actual = g.percursoLargura(0);
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void testG6() throws Exception {
+        Grafo g = new Grafo("g6.txt");
+        List<Integer> expected = new ArrayList<>();
+        for (int i = 0; i <= 9; i++) {
+            expected.add(i);
+        }
+        List<Integer> actual = g.percursoLargura(0);
+        assertEquals(expected, actual);
+    }
+
+    public void testG6Profundidade() throws Exception {
+        Grafo g = new Grafo("g6.txt");
+        String expected = "[0, 1, 2, 5, 6, 3, 7, 4, 8, 9]";
+        String actual = g.percursoProfundidade(0).toString();
+        assertEquals(expected, actual);
+    }
 }
